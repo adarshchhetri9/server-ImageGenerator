5f742e8f5c0f288157c04983ca55e99adc04c69f
adarshchhetri9 <chhetriadarsh912@gmail.com>
2023-01-23 19:34:35 +0530

    till submit error in api

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 import express from 'express';
-
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 
-import connectDB from './mongodb/connect.js'
+import connectDB from './mongodb/connect.js';
 import postRoutes from './routes/postRoutes.js';
 import dalleRoutes from './routes/dalleRoutes.js';
 
@@ -17,22 +16,18 @@ app.use('/api/v1/post', postRoutes);
 app.use('/api/v1/dalle', dalleRoutes);
 
 app.get('/', async (req, res) => {
-  res.send('Hello from DALL.E!'
-  );
+  res.status(200).json({
+    message: 'Hello from DALL.E!',
+  });
 });
 
 const startServer = async () => {
-
-    try {
-        connectDB(process.env.MONGODB_URL);
-        app.listen(8080, () => console.log('Server started on port 8080 http://localhost:8080'));
-    } catch (error) {
-        console.log(error)
-        
-    }
-
-
- 
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8080, () => console.log('Server started on port 8080 http://localhost:8080'));
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 startServer();
